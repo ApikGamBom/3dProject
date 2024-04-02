006db684c0db9741767f742ff7f9d0b2224293cf
electric663hair <electric663hair@gmail.com>
2024-04-02 17:09:21 +0200

    Many changes done
    I added a new scene to try fix movement, added a folder with Dani's movement. and more

--- Project3D/Assets/MouseLook.cs
+++ Project3D/Assets/MouseLook.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+public class mouseLook : MonoBehaviour
 {
     
-    public float mouseSensitivity = 100f; //Gjør at du kan endre muse sensitivitet i denne variabelen her
-
+    public float mouseSensitivity = 100f;
+    
+    public Transform playerBody;
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("mouse X") * mouseSensitivity * Time.deltaTime; //Lager variabel som tracker hvor musen er på X aksen, ganger med sensitiviteten for at man kan endre sensen senere, alt som skjer i "void update()" blir gjort en gang per frame så vi ganger med time.deltatime for at ikke folk med høyere fps får advatage eller disadvantage
-        float mouseY = Input.GetAxis("mouse Y") * mouseSensitivity * Time.deltaTime; //Det samme som linjen over bare at det er Y aksen til musen den detecter og ikke X aksen
+        float mouseX = UnityEngine.Input.GetAxis("MouseX") * mouseSensitivity * Time.deltaTime;
+        float mouseY = UnityEngine.Input.GetAxis("MouseY") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
